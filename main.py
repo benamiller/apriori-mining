12e598124b7f791b6a3a8ff3cd89641ae4253fc4
benamiller <mrbenamiller@gmail.com>
2025-01-24 13:56:02 +0000

    Support makes more sense than frequency in this context

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 filename = "A-NUHa0sQ1C0nBpyRUTG5g_3260c54dad114a1a96af95bf746f9df1_categories.txt"
 
 items = []
-item_to_frequency = {}
+item_to_support = {}
 
 with open(filename, 'r') as file:
     for line in file:
@@ -9,15 +9,16 @@ with open(filename, 'r') as file:
         items.extend(words)
 
         for word in words:
-            if word in item_to_frequency:
-                item_to_frequency[word] += 1
+            if word in item_to_support:
+                item_to_support[word] += 1
             else:
-                item_to_frequency[word] = 1
+                item_to_support[word] = 1
 
     print(items[:10])
-    print(item_to_frequency['Shopping'])
-    print(item_to_frequency['Restaurants'])
-    print(min(item_to_frequency.values()))
-    print(max(item_to_frequency.values()))
+    print(item_to_support['Shopping'])
+    print(item_to_support['Restaurants'])
+    print(min(item_to_support.values()))
+    print(max(item_to_support.values()))
 
+for item, support in item_to_support.items():
 
